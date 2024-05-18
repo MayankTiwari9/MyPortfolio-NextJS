da9813da8c363af080ac8a3d4ce4569cca674ba7
MayankTiwari9 <mt1027319@gmail.com>
2024-05-18 11:25:47 +0530

    Contact Done

--- src/app/Components/Footer/Footer.js
+++ src/app/Components/Footer/Footer.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div id="contact" className="bg-gray-900 text-white">
+      <h1 className="text-3xl mx-auto font-thin tracking-tight sm:text-4xl flex justify-center p-4">
+          Contact Me
+        </h1>
+        <p className='flex mx-auto p-3 w-9/12'>Thank you for visiting my portfolio! If you're looking for a dedicated and skilled Front-End or React Developer to join your team full-time, I'd love to hear from you. Please feel free to contact me at mt1027319@gmail.com to discuss potential opportunities. I look forward to the possibility of contributing to your projects and driving innovation together.</p>
     </div>
   )
 }
